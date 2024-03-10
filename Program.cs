ddb657b8f8a1074e6607707f7d6877750fc9d823
samanthaj7265 <samanthaj7265@gmail.com>
2024-03-10 17:31:28 -0500

    Initial Commit

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿/*
+Download initial movie data file
+Create movie Console Application to see all movies in the file and to add movies to the file.
+Implement Exception Handling
+Implement NLog framework
+Extra: Check for duplicate values
+*/
+
+//Create a menu
+using System.Formats.Asn1;
+using System.Security.Cryptography.X509Certificates;
+using Microsoft.Win32.SafeHandles;
+using NLog.LayoutRenderers.Wrappers;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        //Create a Menu
+        Console.WriteLine("Enter 1 to see list of movies.");
+        Console.WriteLine("Enter 2 to add movie to file.");
+        Console.WriteLine("Enter anything else to quit.");
+        
+    }
+}
